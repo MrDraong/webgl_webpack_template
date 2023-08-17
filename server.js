e3781b6d6d929c772fcf572aa9903074796991db
MrDraong <62282258+MrDraong@users.noreply.github.com>
2023-08-17 13:48:52 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/static', express.static('build/static'));
+app.use(express.static('build/'));
+
+app.listen(8080, () => {
+  console.log('server started at 8080');
+});
